62629028ac6696aef5a3073478f66fe24082e301
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-12-10 10:41:28 +0800

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Home = () => {
+    const users = useLoaderData();
+    const [displayUser, setDisplayUsers] = useState(users);
+    const handleDelete = user => {
+        const agree = window.confirm(`are you sure to delete the name ${user.name}`)
+        console.log(agree)
+        if (agree) {
+            // console.log('delete data',user._id)
+            fetch(`http://localhost:5000/users/${user._id}`, {
+                method: 'DELETE'
+            })
+            .then(res=>res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert('User delete successfully')
+                        const remainingUser = displayUser.filter(usr => usr._id !== user._id);
+                        setDisplayUsers(remainingUser);
+                        
+                    }
+                })
+        }
+    }
+    return (
+        <div>
+            <h2>Users :{displayUser.length}</h2>
+            <div>
+                {
+                    displayUser.map(user => <p key={user._id}>
+                        {user.name}
+                        {user.email}
+                        <button onClick={() => handleDelete(user)}>X</button>
+                    </p>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Home;
